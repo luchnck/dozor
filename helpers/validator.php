c341d9bda8f9bf5e2ae50f25059c186cb8d9d589
Администратор <@KNPZ-NET>
2016-03-21 11:25:35 +0300

    Создан класс helpers\validator как обертка для PEAR::Validate, на стадии прикрутки к models\objects\team.php --> Добавлены функции валидации по значению, пустоте и соответствию определенным данным --> Не протестированы функции этих валидаций

--- helpers/validator.php
+++ helpers/validator.php
@@ -23,6 +23,8 @@ class validator {
     
     //параметры совпадений по равенству значений
     private $equals;
+    
+    public $validationInfo;
     /*
      *      параметры сохраняем ввиде массива
      *          'match' => соответствие определенному типу
@@ -43,7 +45,63 @@ class validator {
     
     public function __construct($parameters = Array()){
         $this->validate = new Validate();
-        $this->parameters = $parameters;
+        $validationInfo = '';
+        
+        foreach($parameters as $key => $value)
+            $this->$key = $value;
     }
     
+    
+    /*
+     *        принимается название значения и значение
+     *        проверяем по всем трем параметрам
+     *        возвращаем логический ответ 
+     */
+    public function validate($key,$value){
+        $match = $this->checkMatch($key,$value);
+        $empty = $this->checkNotEmpty($key,$value);
+        $equals = $this->checkEquals($key,$value);
+        return ($match&&$empty&&$equals);
+    }
+    
+    private function checkMatch($key,$value){
+        if (array_key_exists($key, $this->match)){
+            $params = $this->match[$key];
+            
+            if ($this->validate->$params['type']($value, $params['options']))
+                return true;
+            else {
+                $this->validationInfo .= "в $key используются символы не из ".$params['options']; 
+                return false;
+            }
+        } else
+            return true;
+    }
+    
+    private function checkNotEmpty($key,$value){
+        if (array_key_exists($key, $this->notEmpty)){
+                        
+            if (!empty($value))
+                return true;
+            else {
+                $this->validationInfo .= "$key не должен быть пустым";
+                return false;
+            }
+        } else
+            return true;
+    }
+    
+    private function equals($key,$value){
+        if (array_key_exists($key, $this->equals)){
+            $params = $this->equals[$key];
+            
+            if ($value === $params[$key])
+                return true;
+            else {
+                $this->validationInfo .= "$key не совпадает с полем $params[$key]"; 
+                return false;
+            }
+        } else
+            return true;
+    }
 }
--- models/objects/team.php
+++ models/objects/team.php
@@ -18,6 +18,8 @@ class team {
     
     public $pass;
     
+    public $verpass;
+    
     public $email;
     
     public $validationErrors;
@@ -25,6 +27,60 @@ class team {
     private $validator;
     
     private $rightValues;
+    
+    public function validate(){
+        $return = true;
+        $this->rightValues = 
+        Array(
+                'match' =>  Array(
+                                'name'  => Array(
+                                              'type' => 'string',
+                                              'options' => Array(
+                                                    'format' => VALIDATE_NUM . VALIDATE_ALPHA,
+                                                    'min_length' => 6,
+                                                  )
+                                          ),
+                                'id'    => Array(
+                                              'type' => 'number',
+                                              'options' => Array(),
+                                          ),
+                                'email' => Array(
+                                              'type' => 'email',
+                                              'options' => Array(
+                                                    'check_domain' => 'false',
+                                                    'fullTLDValidation' => 'false',
+                                                    'use_rfc822' => 'true',
+                                                    'VALIDATE_GTLD_EMAILS' => 'true',
+                                                    'VALIDATE_CCTLD_EMAILS' => 'true',
+                                                    'VALIDATE_ITLD_EMAILS' => 'true',
+                                                    ),
+                                          ),
+                                'pass'  => Array(
+                                              'type' => 'string',
+                                              'options' => Array(
+                                                    'format' => VALIDATE_NUM . VALIDATE_ALPHA . VALIDATE_PUNCTUATION,
+                                                    'min_length' => 6,
+                                                  ),
+                                          )
+                                ),
+            
+                'notEmpty' =>Array('name','pass','email'),
+            
+                'equals' => Array(
+                                  'pass' => $this->verpass,
+                                ),
+                );
+        $this->validator = new validator($this->rightValues);
+        
+        foreach (Array('name','pass','email') as $key) {
+            $return = ($return && $this->validator->validate($key, $this->$key));
+        }
+        
+        if (!$return)
+            $this->validationErrors = $this->validator->validationInfo;
+        
+        return $return;            
+    }
     
     public function loadData($array){
         foreach ($array as $key => $value){
